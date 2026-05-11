b35fdf539446fa6848bbbc970209a88edcb41b8f
anshul3212 <159529188+anshul3212@users.noreply.github.com>
2026-05-11 10:32:33 +0530

    Change page component to redirect to login
    Refactor page component to redirect to login instead of rendering UI.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
-"use client"
+// "use client"
 
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
-const page = () => {
+// const page = () => {
 
-  const router = useRouter();
-  return (
-    <div>
-      {/* bg-linear-to-l from-[#A34E25] via-[#734C82] to-[#522762] */}
-      <div className="flex items-center justify-center mt-10">
-        <button className="px-8 py-6 bg-[#3f013a] text-lg text-white cursor-pointer" onClick={()=>{router.push("/login")}}>login</button>
-      </div>
-    </div>
-  );
+//   const router = useRouter();
+//   return (
+//     <div>
+//       {/* bg-linear-to-l from-[#A34E25] via-[#734C82] to-[#522762] */}
+//       <div className="flex items-center justify-center mt-10">
+//         <button className="px-8 py-6 bg-[#3f013a] text-lg text-white cursor-pointer" onClick={()=>{router.push("/login")}}>login</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default page;
+import { redirect } from "next/navigation";
+
+export default function page() {
+  redirect("/login");
+  return null;
 }
-
-export default page;
